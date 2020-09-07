dd8e5be7bf9e479f2fc62ca10ddf680103eb26fc
DAMI KOMOLAFE <damik94@gmail.com>
2020-09-07 14:45:38 -0400

    deployed back end

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 const data = [
     {
         url: "https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-woman-in-a-pool-1259-large.mp4",
-        channel: "@ssasangha",
-        song: "Get 6 figs in 5 years",
+        channel: "@sasha",
+        song: "summer breeze",
         likes: 180,
         messages: 550,
         description: "EPIC MERN Tik-Tok clone",
@@ -10,7 +10,7 @@ const data = [
     },
     {
         url: "https://assets.mixkit.co/videos/preview/mixkit-young-sportsman-jumping-rope-at-home-5050-large.mp4",
-        channel: "@ssasangha",
+        channel: "@fitness-guru",
         song: "Waste my hate on you",
         likes: 140,
         messages: 25,
@@ -19,7 +19,7 @@ const data = [
     },
     {
         url: "https://assets.mixkit.co/videos/preview/mixkit-panting-dog-1548-large.mp4",
-        channel: "@ssasangha",
+        channel: "@good boy",
         song: "Learn to be great",
         likes: 120,
         messages: 2500,
